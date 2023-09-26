eecbb9b4f348d430e4877771e26214328ce6465f
sulaiman <sulaiman@pop-os.localdomain>
2023-09-26 12:06:50 +0530

    funtion &&

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+//&& oprator
     let age = 60;
     if(age>=18 && age<=50){
         console.log('you are eligible to vote')
@@ -6,4 +7,21 @@
     else{
         console.log('not eligible for vote')
     }
+
+
+
+    // && oprator           { && and operator }
+
+function voting(age){
+    if(age>=18 && age<=30){
+        console.log("eligible for vote")
+    }
+    else{
+        console.log('not now')
+    }
+
+}
+voting()
+
+//
 
